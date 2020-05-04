692f7e8a9cc65e6d79797257651d45edb3f5c43d
Kyle Davis <kyled871@yahoo.com>
2020-05-04 17:22:46 -0400

    linked search box to queryURL and added submit function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,10 @@ let apiKey = '&api_key=J8OYNkQOJSuZ9QnbjoWVnOHzl9tGj6Wl'
 
 $('#submitButton').on('click', function() {
 
-    let musician = $(this).attr("data-musician")
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + musician + apiKey;
+    
+    let q = $('#searchBox').val();
+
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + q + apiKey + "&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -15,13 +17,22 @@ $('#submitButton').on('click', function() {
 
     .then(function(response) {
         let results = response.data;
-        console.log(results)
 
+        for (let i = 0; i < results.length; i++) {
 
+            let gifDiv = $('#gifDisplay');
+            let rating = results[i].rating;
+            let p = $('p').text("Rating: " + rating)
+            let gifImage = $("<img>")
+
+            gifImage.attr('src', results[i].images.fixed_height.url)
+
+            gifDiv.prepend(p)
+            gifDiv.prepend(gifImage)
+
+            $("#gifDisplay").prepend(gifDiv);
+        }
     })
-
-
-
 })
 
 
