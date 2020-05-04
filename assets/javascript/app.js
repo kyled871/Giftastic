acb2bd2c3f3c09501aee0fb65e1e634779a6e3de
Kyle Davis <kyled871@yahoo.com>
2020-05-04 16:26:48 -0400

    fixed error jquery link BEFORE bootstrap

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
 
+let apiKey = '&api_key=J8OYNkQOJSuZ9QnbjoWVnOHzl9tGj6Wl'
 
-    
+
+$('#submitButton').on('click', function() {
+
+    let musician = $(this).attr("data-musician")
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + musician + apiKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(response) {
+        let results = response.data;
+        console.log(results)
+
+
+    })
+
+
+
+})
+
 
 
 
