fd337be5af5dcc8f734a98e51f4ec139693a8977
Kyle Davis <kyled871@yahoo.com>
2020-05-06 15:27:36 -0400

    added attr to each button being added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,49 +4,53 @@ $(document).ready(function() {
 
     let apiKey = '&api_key=J8OYNkQOJSuZ9QnbjoWVnOHzl9tGj6Wl'
 
-
     console.log(searchResults)
 
     // displays example search results with array
     $.each(searchResults, function(index, value) {
+        $('#gifButtons').attr('data-name')
         $('#gifButtons').append('<button class="btn btn-light m-1">' + value + '</button>')
     })
 
 
-    $('#submitButton').on('click', function() {
+    $('#gifButtons').on('click', function() {
+        let musician = $(this).attr('data-name')
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + musician + apiKey + "&limit=10";
 
-        // takes value in search box and push it to existing array and appends that value as a new button.
-        let q = $('#searchBox').val().trim();
-        searchResults.push(q)
-        $('#gifButtons').append('<button class="btn btn-light m-1">' + q + '</button>')
-
-
-        
-        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + q + apiKey + "&limit=10";
-        
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-
-
+    
+    
         .then(function(response) {
             let results = response.data;
-
+    
             for (let i = 0; i < results.length; i++) {
-
+    
                 let gifDiv = $('#gifDisplay');
                 let rating = results[i].rating;
                 let p = $('p').text("Rating: " + rating)
                 let gifImage = $("<img>")
-
+    
                 gifImage.attr('src', results[i].images.fixed_height.url)
-
+    
                 gifDiv.prepend(p)
                 gifDiv.prepend(gifImage)
-
+    
                 $("#gifDisplay").prepend(gifDiv);
             }
         })
     })
+
+
+    $('#submitButton').on('click', function(event) {
+        event.preventDefault();
+        // takes value in search box and push it to existing array and appends that value as a new button.
+        let q = $('#searchBox').val().trim();
+        searchResults.push(q)
+        $('#gifButtons').append('<button class="btn btn-light m-1">' + q + '</button>')
+        $('#gifButtons').attr('data-name', q)
+    })
+
 })
